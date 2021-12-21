c05eb1ed88ca7effc7de411c5ddf150f59481dc6
irfani12 <96363883+irfani12@users.noreply.github.com>
2021-12-21 12:12:06 +0800

    update data lokasi
    update data lokasi

---
+++ update_data_lokasi.sql
@@ -0,0 +1,6 @@
+UPDATE lokasi SET nama_lokasi = 'Kota Banjarmasin' WHERE
+id = 1;
+UPDATE lokasi SET nama_lokasi = 'Kota Banjarbaru' WHERE
+id = 2;
+UPDATE lokasi SET nama_lokasi = 'Kabupaten Banjar' WHERE
+id = 3; 
