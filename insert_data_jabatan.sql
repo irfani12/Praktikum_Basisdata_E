8846262d504836c25ab7de2bb94c2cd4aa68683a
irfani12 <96363883+irfani12@users.noreply.github.com>
2021-12-21 12:33:24 +0800

    insert data jabatan
    insert data jabatan

---
+++ insert_data_jabatan.sql
@@ -0,0 +1,8 @@
+INSERT INTO jabatan (id, nama_jabatan,
+gapok_jabatan, tunjangan_jabatan, uang_makan_perhari)
+VALUES
+(NULL, 'System Analyst', 2400000, 500000, 40000),
+(NULL, 'Project Manager', 2300000, 450000, 38000),
+(NULL, 'Senior Programmer', 2200000, 400000, 36000),
+(NULL, 'Junior Programmer', 2100000, 350000, 34000),
+(NULL, 'Intern', 1000000, 100000, 20000);
